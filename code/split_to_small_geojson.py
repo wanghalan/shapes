41868a1bcd0243be372323d4906840117603c969
alan <ahw9f@virginia.edu>
2023-07-28 01:49:48 -0400

    adding tl_2020_05053_tabblock20.zip

--- code/split_to_small_geojson.py
+++ code/split_to_small_geojson.py
@@ -2,37 +2,37 @@ import pathlib
 import geopandas as gpd
 import os
 from tqdm import tqdm
+import fiona
 
 def export_block_geojsons(shape_dir, export_dir):
     shapes = sorted(pathlib.Path(shape_dir).glob('*.zip'))
 
     pbar = tqdm(shapes)
     for county_file in pbar:
-        gdf = gpd.read_file(county_file)
-        county = gdf['GEOID20'].str[:5].values[0]
-
-        # Assuming if a directory is there, the directory is valid
-        if os.path.isdir(os.path.join(export_dir,county)):
-            continue
-        os.system('mkdir -p %s' % os.path.join(export_dir,county))
-
-        pbar.set_description('Updating county: %s' % county)
-        updated = 0
-        for bg in tqdm(gdf['GEOID20'].str[:12]):
-            try:
+        try:
+            gdf = gpd.read_file(county_file)
+            county = gdf['GEOID20'].str[:5].values[0]
+            pbar.set_description('Updating county: %s' % county)
+            # Assuming if a directory is there, the directory is valid
+            if os.path.isdir(os.path.join(export_dir,county)):
+                continue
+            os.system('mkdir -p %s' % os.path.join(export_dir,county))
+            updated = 0
+            for bg in tqdm(gdf['GEOID20'].str[:12]):
                 export_filename = os.path.join(os.path.join(export_dir,county), '%s.geojson' % bg)
                 if os.path.isfile(export_filename):
                     continue
                 pdf = gdf[gdf['GEOID20'].str[:12] == bg]
                 pdf.to_file(export_filename)
                 updated += 1
-            except Exception as e:
-                # DANGEROUS
-                os.system('rm -rf %s' % os.path.join(export_dir,county))
-                raise
-        # Small routine updates
-        if updated > 0:
-            os.system("git add -A && git commit -m 'adding %s' && git pull && git push" % county_file.name)
+        except Exception as e:
+            # Ran into driver error, skipping and deleting base shapefile and the export dir
+            print('Removing: %s' % county_file.resolve())
+            os.system('rm %s' % county_file.resolve())
+            os.system('rm -rf %s' % os.path.join(export_dir,county))
+            continue
+
+        os.system("git add -A && git commit -m 'adding %s' && git pull && git push" % county_file.name)
 
 if __name__=='__main__':
     shapefile_dir = '../../national_address_database/data/shapefiles'

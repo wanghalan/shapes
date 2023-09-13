21d3210cdaac2da2c415d9be7da8b0b4af0b4b9f
alan <ahw9f@virginia.edu>
2023-09-13 01:32:05 -0400

    only including gun zipped files in manifest

--- code/export_filepath_to_json.py
+++ code/export_filepath_to_json.py
@@ -3,7 +3,7 @@ import json
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    data_files = sorted(pathlib.Path("../data").glob("*"))
+    data_files = sorted(pathlib.Path("../data").glob("*.gz"))
 
     with open("../manifest.json", "w") as f:
         json.dump([str(d)[3:] for d in data_files], f)  # skip initial ../

8ef8444c618fe11d5f71e75528def05816701163
alan <ahw9f@virginia.edu>
2023-09-13 01:30:58 -0400

    updaing manifest to include all files

--- code/export_filepath_to_json.py
+++ code/export_filepath_to_json.py
@@ -3,7 +3,7 @@ import json
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    data_files = sorted(pathlib.Path("../data").glob("*.geojson"))
+    data_files = sorted(pathlib.Path("../data").glob("*"))
 
     with open("../manifest.json", "w") as f:
         json.dump([str(d)[3:] for d in data_files], f)  # skip initial ../
